ce721a6f8092218c9361b09c6b7c88331d8fd31a
CharchitSaraswat <charchit91993@gmail.com>
2023-09-20 17:49:16 -0400

    calculate guidance loss

--- ptp_utils.py
+++ ptp_utils.py
@@ -156,7 +156,7 @@ def diffusion_step(model, controller, latents, context, t, guidance_scale, low_r
     obj_cetroid = get_obj_centroid(centroids, moving_obj, tokens, tokenizer)
     obj_cetroid.requires_grad = True
     latents.requires_grad = True
-    guidance_loss = torch.autograd.grad(outputs=get_guidance_loss(target_pt, obj_cetroid), inputs=latents, allow_unused=True)[0]
+    guidance_loss = torch.autograd.grad(outputs=get_guidance_loss(target_pt, obj_cetroid), inputs=latents, allow_unused=True)
     print("guidance_loss", guidance_loss)
     print("noise_pred_uncond.shape", noise_pred_uncond.shape)
     print("latents.shape", latents.shape)
